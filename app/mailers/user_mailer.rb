462ff3f26bd9e614d7cbf44fe57f25aced1ab8e2
donalquirke <donal.quirke@gmail.com>
2013-06-19 14:21:35 +0100

    Fixed registeration notification email to show the email of latest registeration. Modified text of registration confirmation emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,7 @@ class UserMailer < ActionMailer::Base
   end
   
   def registration_notification(user)
+    @latest_reg = Registerations.last
     @user=user
     @url="http://www.moremindfully.com/login"
     mail(:to => user.email, :subject => "New Registration Received", :from => "accounts@moremindfully.com")
