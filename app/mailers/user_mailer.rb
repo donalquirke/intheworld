142dd2b8e1d6c6d9d2fc2596de470194517eaeeb
donalquirke <donal.quirke@gmail.com>
2013-06-19 14:55:00 +0100

    Added a comment note, changed registration confirmationa registration email content a bit.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,19 @@
 class UserMailer < ActionMailer::Base
-    
+  
+  default :from => "accounts@moremindfully.com"
+  
   def registration_confirmation(user)
     @user=user
     @url="http://www.moremindfully.com/login"
-    mail(:to => user.email, :subject => "Thank you for registering with MoreMindfully.com", :from => "accounts@moremindfully.com")
+    # Note: Call to mail needs to be at the end of th method
+    mail(:to => user.email, :subject => "Thank you for registering with MoreMindfully.com")
   end
   
   def registration_notification(user)
     @latest_reg = Registerations.last
     @user=user
     @url="http://www.moremindfully.com/login"
-    mail(:to => user.email, :subject => "New Registration Received", :from => "accounts@moremindfully.com")
+    # Note: Call to mail needs to be at the end of th method
+    mail(:to => user.email, :subject => "New Registration Received")
   end
 end
