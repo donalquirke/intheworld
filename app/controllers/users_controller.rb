04f331a5a6e5859253961ba1d712ba4afc8e99fd
donalquirke <donal.quirke@gmail.com>
2013-06-29 22:58:28 +0100

    more logger info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -81,7 +81,7 @@ class UsersController < ApplicationController
         @random_i = rand(0..(@selected.count-1)) 
         @daily_intention = @selected[@random_i]  
         #UserMailer.deliver_daily_intention(u,@daily_intention).deliver 
-        #Rails.logger.info ("@random_i #{@random_i}, @selected #{@selected}, @selected_intentions #{@selected_intentions}")
+        Rails.logger.info ("@random_i #{@random_i}, @selected #{@selected}, @selected_intentions #{@selected_intentions}")
         Rails.logger.info ("Sent Daily Intention: #{@daily_intention.header} to #{u.email}")
         Rails.logger.info ("Sent Daily Intention: to #{u.email}")
       end
