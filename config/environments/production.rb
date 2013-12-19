1e27bfe9859ea81fe23bbd54e61b714297324314
donalquirke <donal.quirke@gmail.com>
2013-12-19 18:26:59 +0000

    had commented out wrong line in production.rb re:serving assets from amazon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,12 +14,12 @@ Intheworld::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   
-    config.action_controller.asset_host = "http://moremindfully.s3.amazonaws.com"
+  #config.action_controller.asset_host = "http://moremindfully.s3.amazonaws.com"
 
   #config.action_controller.asset_host = "http://d299dq65xxk511.cloudfront.net"
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  #config.serve_static_assets = false
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
